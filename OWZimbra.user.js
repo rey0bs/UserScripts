9db6f76726719c0fa26d2737ac8781d742461a06
Simon Boyer <simon.boyer@openwide.fr>
2013-05-15 15:09:29 +0200

    Add Zimbra GM 1

---
+++ OWZimbra.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name        OWZimbra
+// @namespace   sboye
+// @include     *mail.openwide.fr*
+// @include     *zimbra2.corp.accelance.fr*
+// @version     1
+// @require http://code.jquery.com/jquery-1.7.2.min.js
+// ==/UserScript==
+
+
+// ==/UserScript==
+(function(){
+	
+
+    /*** UTILS ***/
+    function add_style( rules, media ) {
+        el = document.createElement('style');
+        el.setAttribute( 'type', 'text/css' );
+        for ( i=0; i<rules.length; i++ ) {
+            el.innerHTML += rules[ i ] + "\n";
+        }
+        if ( typeof media != "undefined" ) {
+            el.innerHTML = "@media " + media + " { \n" + el.innerHTML + "} \n";
+        }
+        document.head.appendChild(el);
+    }
+    function replace_url_with_html_links( text ) {
+        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+éèàçùêëôûäâöü&@#\/%?=~_|!:,.;()\[\]]*[-A-Z0-9+éèàçùêëôûäâöü&@#\/%=~_|(\[\]])/ig;
+        return text.replace(exp,"<a href='$1' style='color: #050293; text-decoration: underline;' target='_blank'>$1</a>"); 
+    }
+
+    /*** ZIMBRA ***/
+    var msgs = document.getElementsByClassName('MsgBody-text');
+    if ( msgs ) {
+        for ( i=0; i<msgs.length; i++ ) {
+            msgs[i].innerHTML = replace_url_with_html_links( msgs[i].innerHTML );
+        }
+    }
+    if ( appContextPath && appContextPath == "/zimbra" && self==top) {
+        setInterval(
+            function(){
+                var unread = 0;
+                var folders = $('#main_Mail-parent-FOLDER .DwtTreeItem-Text');
+                for ( i=0; i<folders.length; i++ ) {
+                    var open_pos = folders[i].innerHTML.indexOf(" (") + 2;
+                    
+                    if ( open_pos != 1 && folders[i].innerHTML.indexOf("Brouillons")+2 == 1 && folders[i].innerHTML.indexOf("Spam")+2 == 1) {
+                        var close_pos = folders[i].innerHTML.indexOf(")");
+                        unread += parseInt( folders[i].innerHTML.substr( open_pos, close_pos - open_pos ) );
+                    }
+                }
+                if ( unread == 0 ) {
+                    document.title = "Zimbra";
+                } else {
+                    document.title = "("+unread+") Zimbra";
+                }
+            },
+            2000
+        );
+    }
+
+
+})();
