305374c98f111e70f3ca613a9ad1ec253e17441f
Simon Boyer <simon.boyer@openwide.fr>
2014-04-11 14:24:52 +0200

    Update HeliosVulgaris

--- HeliosVulgaris.user.js
+++ HeliosVulgaris.user.js
@@ -3,7 +3,7 @@
 // @namespace   sboye
 // @include     *helios*
 // @include     *xguard*
-// @version     1.3
+// @version     1.4
 // ==/UserScript==
 (function(){
 
@@ -13,18 +13,19 @@
 	var reg2 = new RegExp("( +)(une?|le|la|votre|notre|leur|ce|cette)( +)([aeiouyéèê]+)", "gim");
 	var reg3 = new RegExp("( +)(une?|le|la|votre|notre|leur|ce|cette)( +)(<i>)(putain|saloperie)(</i>)( de)( +)(qui|que|[a-z]+er|[a-z]+ir|[a-z]+endre)( +)", "gim");
 	for (var i = 0; i < messages.length; i++) {
-		if( Math.random() > 0.6) { 
-			messages[i].innerHTML = messages[i].innerHTML.replace(reg1, " $2 <i>putain</i> de $4");
+		var form = new RegExp("<input|<textarea");
+		if(! form.test(messages[i].innerHTML) ) {
+			if( Math.random() > 0.6) { 
+				messages[i].innerHTML = messages[i].innerHTML.replace(reg1, " $2 <i>putain</i> de $4");
+			}
+			if( Math.random() > 0.6) { 
+				messages[i].innerHTML = messages[i].innerHTML.replace(reg1b, " $2 <i>saloperie</i> de $4");
+			}
+			if( Math.random() > 0.6) { 
+				messages[i].innerHTML = messages[i].innerHTML.replace(reg2, " $2 <i>putain</i> d'$4");
+			}
+			messages[i].innerHTML = messages[i].innerHTML.replace(reg3, " $2 $9 ");
 		}
-		if( Math.random() > 0.6) { 
-			messages[i].innerHTML = messages[i].innerHTML.replace(reg1b, " $2 <i>saloperie</i> de $4");
-		}
-		if( Math.random() > 0.6) { 
-			messages[i].innerHTML = messages[i].innerHTML.replace(reg2, " $2 <i>putain</i> d'$4");
-		}
-		messages[i].innerHTML = messages[i].innerHTML.replace(reg3, " $2 $9 ");
-		//messages[i].innerHTML = messages[i].innerHTML.replace(reg1, "$1$2putain d'$3$4");
-	 // Faire quelque chose avec cellules.item(i) ou cellules[i]
 	}
 	
 	
